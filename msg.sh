45d07fadf3a5db4a9a4679a982efbe697ffd165d
Nathalie Redick <>
2020-09-19 15:34:39 -0400

    more concise

--- msg.sh
+++ msg.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-
-var1=$1
-var2=$2
-
-osascript -e 'tell application "Messages" to send "'"$var1"'" to buddy "'"$var2"'"'
+osascript -e 'tell application "Messages" to send "'"$1"'" to buddy "'"$2"'"'
