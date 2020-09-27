f664b008c3b967e9a22a90bc89a28679f537d25e
Nathalie Redick <>
2020-09-27 15:15:15 -0400

    added comments to msg.sh

--- msg.sh
+++ msg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# buddy must be in your contacts, case sensitive
+# buddy must have iMessage 
+
 osascript -e 'tell application "Messages" to send "'"$1"'" to buddy "'"$2"'"'
 if [ $? -eq 0 ]; then
 	echo "Message sent!"
